cea3424ae86ad4fda09e23e6838817037d10b202
Neil Marks <nam48@duke.edu>
2021-01-28 16:22:04 -0500

    updated interface to add HDL option

--- BloodType.py
+++ BloodType.py
@@ -2,8 +2,11 @@ def interface():
     print("Blood Test Analysis:")
     while 1:
         print("\nOptions")
+        print("1 - HDL")
         print("9 - quit")
         choice = input("Enter an option: ")
+        if choice == "1":
+            hdl_driver();
         if choice == "9":
             return
 def hdl_driver():
@@ -30,3 +33,4 @@ def output_hdl(value):
     return
 
 interface()
+# hdl_driver()
