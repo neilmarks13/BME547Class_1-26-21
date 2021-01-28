c2c2b53e03f5623630ce9440a44f404bc91d9e5c
Neil Marks <nam48@duke.edu>
2021-01-28 16:35:40 -0500

    added LDL interface

--- BloodType.py
+++ BloodType.py
@@ -3,10 +3,13 @@ def interface():
     while 1:
         print("\nOptions")
         print("1 - HDL")
+        print("2 - LDL")
         print("9 - quit")
         choice = input("Enter an option: ")
         if choice == "1":
             hdl_driver();
+        if choice == "2":
+            ldl_driver();
         if choice == "9":
             return
 def hdl_driver():
