8b202cf56db0a5c0af29023cad4f28804594faae
Neil Marks <nam48@duke.edu>
2021-01-28 15:55:19 -0500

    create simple interface

---
+++ BloodType.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Blood Test Analysis:")
+    while 1:
+        print("\nOptions")
+        print("9 - quit")
+        choice = input("Enter an option: ")
+        if choice == "9":
+            return
+
+interface()
