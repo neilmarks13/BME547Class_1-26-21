d746c7946347a2bba097559ced9f4ccc58affced
Neil Marks <nam48@duke.edu>
2021-01-28 16:13:28 -0500

    analyze hdl function added

--- BloodType.py
+++ BloodType.py
@@ -7,11 +7,20 @@ def interface():
         if choice == "9":
             return
 def hdl_driver():
-    
+    data = input_hdl()
+
+    return
 
 def input_hdl():
     data_in = input("Enter HDL data: ")
     return data_in
 
+def analyze_hdl(data):
+    if data>=60:
+        return "Normal"
+    elif data>=40:
+        return "Borderline Low"
+    else:
+        return "Low"
 
 interface()
